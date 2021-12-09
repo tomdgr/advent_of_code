0a64039fc6c61a2c7e325908bd3ab1887bcf82a0
Tom Daniel Grande <TomDaniel.Grande@skatteetaten.no>
2021-12-09 15:22:39 +0100

    oppgave 2 funker ikke :(

--- AdventOfCode/code/luke9.py
+++ AdventOfCode/code/luke9.py
@@ -1,4 +1,5 @@
 import numpy as np
+COUNT=0
 def get_input():
     """
     :input: data_stream_from_task
@@ -27,14 +28,73 @@ def findmin(heightmap):
 
     return total_risk_level
 
+def search(i,j,COUNT,heatmap,Searched):
+    a=0
+    if (heatmap[i][j]==9 or Searched[i][j]==-1):
+        a=1
+
+    else:
+        COUNT+=1
+        Searched[i][j]=-1
+        search(i,j,COUNT,heatmap,Searched)
+
+
+
+
+def calc_areas(heatmap):
+    """
+    Search: 1. FOR all entries: is the entry searched?
+                    if not add to searched.
+                    Find number of adjacent non walls(9's),
+                    are this already searched?
+                    if not: add number of adjacent non walls to area.
+                    recursivly call function in searchable directions
+
+               add new entry to SEARCHED list,
+
+    :return: list of areas found in search algo
+    """
+
+    list_of_Areas=[]
+    Searched=np.zeros((101,101))
+    for i in range(1,len(heatmap)-1):
+        for j in range(1,len(heatmap[0])-1):
+            COUNT=0
+
+            search(i,j,COUNT,heatmap,Searched)
+        list_of_Areas.append(COUNT)
+    return list_of_Areas
+
+def Nmaxelements(list1, N):
+    final_list = []
+
+    for i in range(0, N):
+        max1 = 0
+
+        for j in range(len(list1)):
+            if list1[j] > max1:
+                max1 = list1[j];
+
+        list1.remove(max1);
+        final_list.append(max1)
+
+    print(final_list)
 
 def main():
+    COUNT=0
+    #Task1
     numpyinput=get_input()
     numpyinput=add_border(numpyinput)
     print(numpyinput)
     print(len(numpyinput),len(numpyinput[0]))
     total_risk=findmin(numpyinput)
     print(total_risk)
+
+    #Task2:
+    numpyinput=get_input()
+    numpyinput=add_border(numpyinput)
+    list_of_areas=calc_areas(numpyinput)
+    Nmaxelements(list_of_areas,3)
 if __name__ == "__main__":
     main()
 
